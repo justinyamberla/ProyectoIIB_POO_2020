3c322efc2465dfef75e5b9ad23eeb80c622b1d02
justinyamberla <55857299+justinyamberla@users.noreply.github.com>
2020-09-01 12:40:35 -0500

    Enemigos
    Creación: clases de enemigos v1.0.0

---
+++ ProyectoIIB_POO/src/proyectoiib_poo/Enemigo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoiib_poo;
+
+/**
+ *
+ * @author justi
+ */
+public class Enemigo {
+    private int vida;
+    private int damage;
+    private int turnoActual;
+    
+    public void ataque(){}
+    
+    public void ataqueEspecial(){}
+}
